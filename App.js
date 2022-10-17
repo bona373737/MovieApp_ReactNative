4587e37b73ed61ba0be1af874a49fc4853f62520
bona <bona373737@gmail.com>
2022-10-18 02:54:03 +0900

    MovieApp프로젝트 생성

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import AppLoading from 'expo-app-loading';
+
+
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [ready, setReady] = useState(false);
+  const onFinish = ()=>setReady(true);
+
+  if(!ready){
+    return <AppLoading onFinish={onFinish}/>
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    null
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
---
+++ README.md
@@ -0,0 +1,15 @@
+# MovieApp만들기 
+- TMDB API활용 영화정보 앱 만들기 
+- React Native CLI_create-react-native-app프로젝트 생성
+
+## 윈도우에 react native 개발 환경 구축하기
+https://reactnative.dev/docs/environment-setup   
+- Chocolatey 설치
+- Nodejs 설치
+- React Native CLI 설치
+- JDK 설치
+- 안드로이드 스튜디오 설치
+  - 안드로이드 스튜디오 설정
+  - 안드로이드 스튜디오 SDK 설정
+  - 안드로이드 스튜디오 환경 변수 설정
+  
